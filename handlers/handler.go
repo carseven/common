13601001f217616bf77a10962eb17e7e17143de3
Carles Serra Vendrell <carles.serra.vendrell@hp.com>
2024-01-11 20:20:58 +0000

    feat: add custom headers

--- handlers/handler.go
+++ handlers/handler.go
@@ -2,7 +2,7 @@ package handlers
 
 import "github.com/labstack/echo"
 
-func customResponseHeaders(next echo.HandlerFunc) echo.HandlerFunc {
+func CustomResponseHeaders(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		c.Response().Header().Set(echo.HeaderAccessControlAllowOrigin, "*")
 		c.Response().Header().Set(echo.HeaderAccessControlAllowHeaders, "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT")
